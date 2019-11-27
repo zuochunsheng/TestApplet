77bc25a3200e3c4422394a44ddee82457636dbe1
zuochunsheng <zuochunsheng@ledcas.com>
2019-11-27 17:02:25 +0800

    引用js

---
+++ pages/data/newsdata.js
@@ -0,0 +1,17 @@
+var initData = [
+	{
+		"newsid": 0,
+		"authorIcon": "../../images/coming.png",
+		"authorName": "iwen",
+		"authorDate": "2017/3/21",
+		"title": "无语 - 本兮",
+		"articleImg": "../../images/news.png",
+		"articleText": "忘记曾经的事情，你需要多久，我想了多久，也没能说出口，看开曾经需要多久，我想了几秒钟",
+		"articleLikeComment": "52",
+		"articleLikeSC": "30"
+	}
+]
+
+module.exports = {
+	initData: initData
+}
--- pages/swiper/swiper.js
+++ pages/swiper/swiper.js
@@ -1,17 +1,25 @@
+
+// 引用js 
+var newsData = require("../data/newsdata.js");
+
 Page({
 
 	/**
 	 * 页面的初始数据
 	 */
 	data: {
-		indiatorDots :true
+		indiatorDots :true,
+		userData :""
+
 	},
 
 	/**
 	 * 生命周期函数--监听页面加载
 	 */
 	onLoad: function (options) {
-		
+		this.setData({
+			useData: newsData.initData
+		})
 	},
 
 	/**
