67702ad3b7866983c7288a41cccd1bb3a5b3a4f1
zuochunsheng <zuochunsheng@ledcas.com>
2019-11-29 10:07:37 +0800

    使用腾讯Wxpage

--- pages/tab-bar2/tab-bar2.js
+++ pages/tab-bar2/tab-bar2.js
@@ -1,66 +1,17 @@
 // pages/tab-bar2/tab-bar2.js
-Page({
 
-	/**
-	 * 页面的初始数据
-	 */
-	data: {
-
+var P = require("../../lib/wxpage.js")
+//监听页面
+P("tab-bar2",{
+	data:{
+		 value :'abc'
+	},
+	onLoad:function(event){
+		console.log("bar-bars onLoad")
 	},
 
-	/**
-	 * 生命周期函数--监听页面加载
-	 */
-	onLoad: function (options) {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面初次渲染完成
-	 */
-	onReady: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面显示
-	 */
-	onShow: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面隐藏
-	 */
-	onHide: function () {
-
-	},
-
-	/**
-	 * 生命周期函数--监听页面卸载
-	 */
-	onUnload: function () {
-
-	},
-
-	/**
-	 * 页面相关事件处理函数--监听用户下拉动作
-	 */
-	onPullDownRefresh: function () {
-
-	},
-
-	/**
-	 * 页面上拉触底事件的处理函数
-	 */
-	onReachBottom: function () {
-
-	},
-
-	/**
-	 * 用户点击右上角分享
-	 */
-	onShareAppMessage: function () {
-
+	onPreload:function(event){
+			this.$preLoad("../index/index")
 	}
+
 })
