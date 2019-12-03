ad32db87cbe7c5e284a429d6b4dc5fad379155a8
zuochunsheng <zuochunsheng@ledcas.com>
2019-12-03 09:42:07 +0800

    起止时间选择器

--- pages/paoma/paoma.js
+++ pages/paoma/paoma.js
@@ -18,7 +18,20 @@ Page({
 		callGetPhone: '13761654037',
 		banners: [
 			{ "picUrl": "https://gtd.alicdn.com/sns_logo/i1/TB124_3NXXXXXasXVXXSutbFXXX.jpg_240x240xz.jpg" }, { "picUrl": "https://gtd.alicdn.com/sns_logo/i7/TB1IWtgQFXXXXcmXFXXSutbFXXX.jpg_240x240xz.jpg" }, { "picUrl": "https://gtd.alicdn.com/sns_logo/i1/TB1DX3hIpXXXXXIaXXXSutbFXXX.jpg_240x240xz.jpg" }
-		]
+		],
+		isPickerRender: false,
+		isPickerShow: false,
+		startTime: "2019-01-01 12:32:44",
+		endTime: "2019-12-01 12:32:44",
+		pickerConfig: {
+			endDate: true, // 是否需要结束时间，为true时显示开始时间和结束时间两个picker
+			column: "second",//可选的最小时间范围hour、minute、secend
+			dateLimit: true, //是否现在时间可选范围，false时可选任意时间；未数字n时，范围是当前时间的最近n天
+			initStartTime: "2019-01-01 12:32:44",//picker初始时间，默认当前时间
+			initEndTime: "2019-12-01 12:32:44",//picker初始结束时间，默认当前时间
+			limitStartTime: "2015-05-06 12:32:44",//最小可选时间
+			limitEndTime: "2055-05-06 12:32:44"//最大可选时间 
+		}
 	},
 	onShow: function () {
 		// 页面显示
@@ -69,6 +82,42 @@ Page({
 				console.log("拨打电话失败！")
 			}
 		})
+	},
+	pickerShow: function () {
+		this.setData({
+			isPickerShow: true,
+			isPickerRender: true,
+			chartHide: true
+		});
+	},
+	pickerHide: function () {
+		this.setData({
+			isPickerShow: false,
+			chartHide: false
+		});
+	},
+
+	bindPickerChange: function (e) {
+		console.log("picker发送选择改变，携带值为", e.detail.value);
+		console.log(this.data.sensorList);
+
+		this.getData(this.data.sensorList[e.detail.value].id);
+		// let startDate = util.formatTime(new Date(new Date().getTime() - 24 * 60 * 60 * 1000 * 7));
+		// let endDate = util.formatTime(new Date());
+		this.setData({
+			index: e.detail.value,
+			sensorId: this.data.sensorList[e.detail.value].id
+			// startDate,
+			// endDate
+		});
+	},
+	setPickerTime: function (val) {
+		console.log(val);
+		let data = val.detail;
+		this.setData({
+			startTime: data.startTime,
+			endTime: data.endTime
+		});
 	}
 
 })
